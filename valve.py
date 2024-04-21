9347f62f55b5e068fd5e904e4cd09952a0af858b
Tomek K <tomek1ni@gmail.com>
2024-04-21 14:47:53 +0200

    New valve reverse valve file and file name changes

--- valve.py
+++ valve.py
@@ -1,49 +1,34 @@
-#valve.py
+#valve_old.py
 import RPi.GPIO as GPIO
-import numpy as np
-import board
-import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 from time import sleep
 
-GPIO.setwarnings(False)         # Ignore warning for now
 GPIO.setmode(GPIO.BCM)
 
 sleepTime = .1
 buttonPin = 18
 lightPin = 22
 relay1 = 23
-relay2 = 24
-buttonState = False
-buttonPrev = False
-valveState = False
-
-
-i2c = busio.I2C(board.SCL, board.SDA)       # Create the I2C bus
-ads = ADS.ADS1015(i2c)                      # Create the ADC object using the I2C bus
-chan = AnalogIn(ads, ADS.P1)                # Create single-ended input on channel 3
+relay2 =24
+buttonState = 0
 
 GPIO.setup(lightPin, GPIO.OUT)
 GPIO.setup(relay1, GPIO.OUT)
 GPIO.setup(relay2, GPIO.OUT)
+
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True: # Run forever
-    while GPIO.input(buttonPin) == False:   #While the button is pressed
-        buttonState = not buttonPrev
-    if buttonState != buttonPrev or chan.value < 20000:
-        if valveState == True or chan.value < 20000:              # 
-            GPIO.output(relay1, 1)          # Close the valve
-            GPIO.output(lightPin, 0)        # Turn the LED off
-            valveState = False              # Assign opened state to the valve state value
-        else:
-            GPIO.output(relay1, 0)          # Open the valve
-            GPIO.output(lightPin, 1)        # Turn the LED on
-            valveState = True               # Assign opened state to the valve state value
-        buttonPrev = buttonState
+    if GPIO.input(buttonPin) == 0:
+        buttonState = 0
+        GPIO.output(lightPin, 1)
+        GPIO.output(relay1, 0)
+        GPIO.output(relay2, 0)
+    else:
+        buttonState = 1
+        GPIO.output(lightPin, 0)
+        GPIO.output(relay1, 1)
+        GPIO.output(relay2, 1)
 
-    print("buttonState", buttonState)
-    print("buttonPrev", buttonPrev)
-    print("moisture level: ", chan.value, chan.voltage)
-    sleep(.2)
+    print("buttonState ", buttonState)
+    print("buttonPin ", GPIO.input(buttonPin))
+    sleep(.3)
---
+++ valve_reverse.py
@@ -0,0 +1,67 @@
+#valve_revers.py
+# U.S. Solid Motorized Ball Valve- 1/2” 15mm, 2 Wire Reverse Polarity USS-MSV00012 SKU:JFMSV00012
+# Valve connected to two channel relay in reverse polarity setup powered by 12V DC.
+# Two buttons on the breadboard (green and yellow). The green (GPIO18) opens the valve, yellow (GPIO16) closes.
+# While button held down valve continuously opens/closes until released. One short tap on a button powers the 
+# valve for 4 seconds making it fully open/close. One more press on either button interrupts 'exit.wait (4)' 
+# statement and stops valve from actuating. Within one second after last interrupt event and if the variable
+# 'susp_auto' is set to '1' the 'exit.wait (4)' isn't available. It suppose to prevent situation when after one
+# interrupt event valve goes directly into an other full close/open cycle.
+
+import RPi.GPIO as GPIO
+import board
+from threading import Event
+from time import time
+
+buttonPin1 = 18 # Green button - open
+buttonPin2 = 16 # Yellow button - close
+relay1 = 23
+relay2 = 19
+exit = Event ()
+susp_auto = 0
+susp_start = time ()
+
+GPIO.setup (relay1, GPIO.OUT)
+GPIO.setup (relay2, GPIO.OUT)
+GPIO.setup (buttonPin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup (buttonPin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def quick_press ():                         # Function is executed after short tap on one of the buttons
+    global susp_auto
+    susp_stop = time ()
+    if ((susp_stop - susp_start) > 1) & (susp_auto == 0): 
+        GPIO.add_event_detect (buttonPin1, GPIO.FALLING, callback=button_callback, bouncetime=50)
+        GPIO.add_event_detect (buttonPin2, GPIO.FALLING, callback=button_callback, bouncetime=50)
+        exit.wait (4)
+        GPIO.remove_event_detect (buttonPin1)
+        GPIO.remove_event_detect (buttonPin2)
+    elif ((susp_stop - susp_start) > 1) & (susp_auto == 1):
+        susp_auto = 0
+    exit.clear ()
+
+def button_callback (channel):              # Function executed after button interrupt event in quick_press()
+    global susp_auto
+    exit.set()
+    susp_auto = 1
+    global susp_start
+    susp_start = time ()
+
+def reset_relays ():                        # Function deenergizing the valve
+    GPIO.output (relay1, 1)
+    GPIO.output (relay2, 0)
+    
+reset_relays()
+
+while True: # Run forever
+    if (GPIO.input (buttonPin1) == False) or (GPIO.input (buttonPin2) == False):
+        start_press = time ()
+        while GPIO.input (buttonPin1) == False:
+            GPIO.output (relay1, 0)
+            GPIO.output (relay2, 0)
+        while GPIO.input (buttonPin2) == False:
+            GPIO.output (relay1, 1)
+            GPIO.output (relay2, 1)
+        end_press = time ()
+        if end_press - start_press < 0.15:
+            quick_press ()
+        reset_relays ()
